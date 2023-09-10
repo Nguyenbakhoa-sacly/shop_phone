00836df1714b0252e62557d00b8148f3897f7552
Nguyenbakhoa-sacly <khoana23062002@gmail.com>
2023-09-11 01:19:43 +0700

    set up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
 
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './redux/slides/counterSlice'
+import './app.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './containers/Layout'
+import { Home, Login, Logout, NotFound, Order, Products } from './page'
+
 
 function App() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
 
   return (
-    <div>
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span className='text-3xl'>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/order' element={<Order />} />
+        </Route>
+
+        <Route path='/login' element={<Login />} />
+        <Route path='/logout' element={<Logout />} />
+        <Route path='/*' element={< NotFound />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/page/index.js
@@ -0,0 +1,8 @@
+
+
+export { default as Home } from './homePage/Home'
+export { default as Order } from './orderPage/Order'
+export { default as Products } from './productsPage/Products'
+export { default as Login } from './loginPage/Login'
+export { default as Logout } from './logoutPage/LogOut'
+export { default as NotFound } from './notFoundPage/NotFound'
